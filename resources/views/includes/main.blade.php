abf1e1dc2c2acc6909509326bf4fa67921b694cc
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-06 13:47:00 +0100

    fix main page

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="@yield('icon', asset('images/egg-logo.png'))" type="image/png">
+    <link rel="icon" href="@yield('icon', asset(''))" type="image/png">
     <title>{{ env('APP_NAME') }} | @yield('title')</title>
     @yield('cdns')
     @vite('resources/js/app.js')
