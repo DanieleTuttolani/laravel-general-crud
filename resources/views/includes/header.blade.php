fc562146c5ebd1fdfaf20f2f1054949123bf622c
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-06 14:15:36 +0100

    fix and rename img

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="#">
-        <img src="{{ asset('images/egg-logo.png') }}" width="30" height="30" class="d-inline-block align-top"
-            alt="">
+        <img src="{{ asset('logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
         myHospital
     </a>
 </nav>
