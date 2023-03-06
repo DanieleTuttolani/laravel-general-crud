8b1d3a8e8b2c32b95a8f2078bebe450affe8e437
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-06 14:55:58 +0100

    fillable protected value

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Guest extends Model
 {
     use HasFactory;
+    protected $fillable =['name', 'surname' , 'born', 'gender' , 'age', 'birth-place', 'CF', 'doc-number','printing-date', 'piastrine', 'GB', 'GR', 'EMO', 'EMA', 'VCM', 'CCM','CMC','AMD','VMP','EMI','ALT'];
 }
--- database/seeders/GuestSeeder.php
+++ database/seeders/GuestSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Guest;
 
 class GuestSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class GuestSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
     }
 }
