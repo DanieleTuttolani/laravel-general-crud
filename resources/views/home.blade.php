4119f30d8fc508b8aef06724004d1f819c58b9e1
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-06 13:59:24 +0100

    add example h1 title in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,5 @@
 @section('content')
     <div class="title-content">
         <h1 class="h-100 p-5 title-text d-flex justify-content-center align-items-center">Benvenuto nella Home</h1>
-
     </div>
 @endsection
